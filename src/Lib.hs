41abea359fc4ec438892c9c32ecdd273a484b63f
ocramz <zocca.marco@gmail.com>
2016-07-30 13:11:36 +0200

    ast -> graph

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,32 @@
-module Lib
-    ( someFunc
-    ) where
+{-# language TypeFamilies #-}
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+class MuRef a where
+  type DeRef a :: * -> *
+  mapDeRef :: Applicative f => (a -> f u) -> a -> f (DeRef a u)
+
+
+
+-- --
+  
+
+type Name = String
+data TreeAst = Var Name
+             | Const Int
+             | UnOp TreeAst
+             | BinOp TreeAst TreeAst deriving (Eq, Show)
+
+data GraphAst s = GVar Name
+                | GConst Int
+                | GUnOp s
+                | GBinOp s s               
+               
+
+instance MuRef TreeAst where
+  type DeRef TreeAst = GraphAst
+  mapDeRef _ (Var v) = pure $ GVar v
+  mapDeRef _ (Const c) = pure $ GConst c
+  mapDeRef f (UnOp p) = GUnOp <$> f p
+  mapDeRef f (BinOp p1 p2) = GBinOp <$> f p1 <*> f p2
+  
